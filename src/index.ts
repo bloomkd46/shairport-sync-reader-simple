4381130b79a7f70dc72ec651ed90e6b83a0cf0c4
Kolton Bloom <bloomkd46@icloud.com>
2025-07-11 16:50:42 -0700

    fixed a bug causing newlines to break the parser

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,9 @@ export default class ShairportSyncReaderSimple {
   private handleData(data: string) {
     this.xml += data;
 
+    // Normalize the XML string by removing line breaks
+    this.xml = this.xml.replace(/\r?\n|\r/g, '');
+
     const itemRegex = /<item><type>(.*?)<\/type><code>(.*?)<\/code><length>(\d+)<\/length><data encoding="base64">(.*?)<\/data><\/item>/g;
     let match;
 
